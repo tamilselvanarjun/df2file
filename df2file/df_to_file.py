1294144e0eb6f1f3314040bf72b5727b26a677c5
Tamilselvan A <42732673+tamilselvanarjun@users.noreply.github.com>
2018-05-15 01:02:00 +0530

    fixed the code with new changes

--- df2file/df_to_file.py
+++ df2file/df_to_file.py
@@ -3,76 +3,76 @@ import pandas as pd
 from openpyxl import load_workbook
 
 def append_df_to_excel(filename, df, sheet_name='Sheet1', startrow=None,
-                       truncate_sheet=False,
-                       **to_excel_kwargs):
-       # Excel file doesn't exist - saving and exiting
-    if not filename.endswith('xlsx'):
+                       truncate_sheet=False, **to_excel_kwargs):
+    """
+    Append a DataFrame to an existing Excel file or create a new one if the file doesn't exist.
+
+    Parameters:
+    - filename (str): Path to the Excel file.
+    - df (DataFrame): DataFrame to be appended.
+    - sheet_name (str): Name of the Excel sheet.
+    - startrow (int): Starting row for appending the DataFrame.
+    - truncate_sheet (bool): If True, truncate the sheet before appending.
+    - to_excel_kwargs (dict): Additional arguments for DataFrame to_excel method.
+
+    Returns:
+    - None
+    """
+    # Ensure filename ends with '.xlsx'
+    if not filename.endswith('.xlsx'):
         filename = filename + '.xlsx'
+
+    # Save DataFrame to Excel if file doesn't exist
     if not os.path.isfile(filename):
-        df.to_excel(
-            filename,
-            sheet_name=sheet_name,
-            startrow=startrow if startrow is not None else 0,
-            **to_excel_kwargs)
+        df.to_excel(filename, sheet_name=sheet_name, startrow=startrow if startrow is not None else 0, **to_excel_kwargs)
         return
-   
-    # ignore [engine] parameter if it was passed
-    if 'engine' in to_excel_kwargs:
-        to_excel_kwargs.pop('engine')
 
+    # Open existing workbook
     writer = pd.ExcelWriter(filename, engine='openpyxl', mode='a')
-
-    # try to open an existing workbook
     writer.book = load_workbook(filename)
-   
-    # get the last row in the existing Excel sheet
-    # if it was not specified explicitly
+
+    # Get the last row in the sheet or set startrow if not specified
     if startrow is None and sheet_name in writer.book.sheetnames:
         startrow = writer.book[sheet_name].max_row
 
-    # truncate sheet
+    # Truncate sheet if specified
     if truncate_sheet and sheet_name in writer.book.sheetnames:
-        # index of [sheet_name] sheet
         idx = writer.book.sheetnames.index(sheet_name)
-        # remove [sheet_name]
         writer.book.remove(writer.book.worksheets[idx])
-        # create an empty sheet [sheet_name] using old index
         writer.book.create_sheet(sheet_name, idx)
-   
-    # copy existing sheets
-    writer.sheets = {ws.title:ws for ws in writer.book.worksheets}
 
-    if startrow is None:
-        startrow = 0
+    # Copy existing sheets
+    writer.sheets = {ws.title: ws for ws in writer.book.worksheets}
 
-    # write out the new sheet
+    # Write DataFrame to Excel
     df.to_excel(writer, sheet_name, startrow=startrow, **to_excel_kwargs)
 
-    # save the workbook
-    writer.save()    
-	
-	
-#append_df_to_excel(output, df, all_div.h1.text, index=False)
+    # Save the workbook
+    writer.save()
 
 
 def append_df_to_csv(filename, df, header=True, index=False, **to_csv_kwargs):
-    # CSV file doesn't exist - saving and exiting
+    """
+    Append a DataFrame to an existing CSV file or create a new one if the file doesn't exist.
+
+    Parameters:
+    - filename (str): Path to the CSV file.
+    - df (DataFrame): DataFrame to be appended.
+    - header (bool): Whether to write header row.
+    - index (bool): Whether to write index.
+
+    Returns:
+    - None
+    """
+    # Ensure filename ends with '.csv'
     if not filename.endswith('.csv'):
         filename = filename + '.csv'
-    if not os.path.isfile(filename):
-        df.to_csv(filename, header=header, index=index, **to_csv_kwargs)
-        return
 
-    # ignore [mode] parameter if it was passed
-    if 'mode' in to_csv_kwargs:
-        to_csv_kwargs.pop('mode')
-
-    # append to existing CSV file
+    # Append DataFrame to CSV file
     df.to_csv(filename, mode='a', header=header, index=index, **to_csv_kwargs)
 
 
-#output_csv = 'output.csv'
-#df = pd.DataFrame({'Column1': [4, 5, 6], 'Column2': ['D', 'E', 'F']})
-
-# Append DataFrame to CSV file without writing header and index
-#append_df_to_csv(output_csv, df, header=False, index=False)
+# Example usage:
+# output_csv = 'output.csv'
+# df = pd.DataFrame({'Column1': [4, 5, 6], 'Column2': ['D', 'E', 'F']})
+# append_df_to_csv(output_csv, df, header=False, index=False)
