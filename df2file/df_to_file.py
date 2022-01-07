0ed5d158b86bfcdde3c9a39e72ae3141ab158752
avataylor99 <vlaazzpppbamml@hotmail.com>
2022-01-07 10:18:20 +0500

    Implement feature

--- df2file/df_to_file.py
+++ df2file/df_to_file.py
@@ -44,7 +44,7 @@ def append_df_to_excel(filename, df, sheet_name='Sheet1', startrow=None,
     # Copy existing sheets
     writer.sheets = {ws.title: ws for ws in writer.book.worksheets}
 
-    # Write DataFrame to Excel
+   
     df.to_excel(writer, sheet_name, startrow=startrow, **to_excel_kwargs)
 
     # Save the workbook
