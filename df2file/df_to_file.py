449bc5339b5332077186a9c928483663c7c3b19a
benjaminn10 <xnarrrcoamlykx@hotmail.com>
2022-11-17 10:18:20 +0500

    Remove redundant code

--- df2file/df_to_file.py
+++ df2file/df_to_file.py
@@ -72,7 +72,4 @@ def append_df_to_csv(filename, df, header=True, index=False, **to_csv_kwargs):
     df.to_csv(filename, mode='a', header=header, index=index, **to_csv_kwargs)
 
 
-# Example usage:
-# output_csv = 'output.csv'
-# df = pd.DataFrame({'Column1': [4, 5, 6], 'Column2': ['D', 'E', 'F']})
-# append_df_to_csv(output_csv, df, header=False, index=False)
+
